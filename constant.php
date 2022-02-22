e0ddca6489da6534602a33d7a7674c46a6688199
Dhanashree1065 <42116126+Dhanashree1065@users.noreply.github.com>
2022-02-22 21:46:54 +0530

    Add files via upload

---
+++ constant.php
@@ -0,0 +1,12 @@
+<?php
+
+   define("NAME","Dhanashree Pathare"); //third parameter "true/false" doesn't work. use for case-sensitive
+   echo NAME."<br>";
+
+   const message= "Hello Jaya"; //constant are case-sensitive
+   echo message; 
+
+
+
+
+?>
